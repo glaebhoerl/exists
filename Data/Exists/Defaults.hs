2b21ca1cd110f9e2abde0db33ac10afe89a1f4b1
illissius <illissius@gmail.com>
2011-11-27 18:52:32 +0100

    use convenience aliases in Defaults

--- Data/Exists/Defaults.hs
+++ Data/Exists/Defaults.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE GADTs, TypeFamilies, Rank2Types, ConstraintKinds  #-}
+{-# LANGUAGE GADTs, TypeFamilies, Rank2Types, ConstraintKinds, FlexibleContexts  #-}
 
 -- | Default functions which can be used as method implementations when writing type class instances for existential datatypes.
 module Data.Exists.Defaults where
@@ -23,99 +23,99 @@ import Control.Comonad.Store.Class    (ComonadStore  (..))
 import Data.Copointed                 (Copointed     (..))
 
 -- * Prelude.Show
-showDefault :: (Existential e, ConstraintOf e ~ Show) => e -> String
+showDefault :: ExistentialWith Show e => e -> String
 showDefault = apply show
 {-# INLINE showDefault #-}
 
-showsPrecDefault :: (Existential e, ConstraintOf e ~ Show) => Int -> e -> ShowS
+showsPrecDefault :: ExistentialWith Show e => Int -> e -> ShowS
 showsPrecDefault n e s = apply (\a -> showsPrec n a s) e
 {-# INLINE showsPrecDefault #-}
 
 -- * Prelude.Functor
-fmapDefault :: (Existential1 e, ConstraintOf1 e ~ Functor) => (a -> b) -> e a -> e b
+fmapDefault :: ExistentialWith1 Functor e => (a -> b) -> e a -> e b
 fmapDefault f = apply1 (exists1 . fmap f)
 {-# INLINE fmapDefault #-}
 
 -- * Data.Foldable.Foldable
-foldDefault :: (Existential1 e, ConstraintOf1 e ~ Foldable, Monoid m) => e m -> m
+foldDefault :: (ExistentialWith1 Foldable e, Monoid m) => e m -> m
 foldDefault = apply1 fold
 {-# INLINE foldDefault #-}
 
-foldMapDefault :: (Existential1 e, ConstraintOf1 e ~ Foldable, Monoid m) => (a -> m) -> e a -> m
+foldMapDefault :: (ExistentialWith1 Foldable e, Monoid m) => (a -> m) -> e a -> m
 foldMapDefault f = apply1 (foldMap f)
 {-# INLINE foldMapDefault #-}
 
-foldrDefault :: (Existential1 e, ConstraintOf1 e ~ Foldable) => (a -> b -> b) -> b -> e a -> b
+foldrDefault :: ExistentialWith1 Foldable e => (a -> b -> b) -> b -> e a -> b
 foldrDefault f x = apply1 (foldr f x)
 {-# INLINE foldrDefault #-}
 
-foldlDefault :: (Existential1 e, ConstraintOf1 e ~ Foldable) => (a -> b -> a) -> a -> e b -> a
+foldlDefault :: ExistentialWith1 Foldable e => (a -> b -> a) -> a -> e b -> a
 foldlDefault f x = apply1 (foldl f x)
 {-# INLINE foldlDefault #-}
 
-foldr1Default :: (Existential1 e, ConstraintOf1 e ~ Foldable) => (a -> a -> a) -> e a -> a
+foldr1Default :: ExistentialWith1 Foldable e => (a -> a -> a) -> e a -> a
 foldr1Default f = apply1 (foldr1 f)
 {-# INLINE foldr1Default #-}
 
-foldl1Default :: (Existential1 e, ConstraintOf1 e ~ Foldable) => (a -> a -> a) -> e a -> a
+foldl1Default :: ExistentialWith1 Foldable e => (a -> a -> a) -> e a -> a
 foldl1Default f = apply1 (foldl1 f)
 {-# INLINE foldl1Default #-}
 
 -- * Data.Traversable.Traversable
-traverseDefault :: (Existential1 e, ConstraintOf1 e ~ Traversable, Applicative f) => (a -> f b) -> e a -> f (e b)
+traverseDefault :: (ExistentialWith1 Traversable e, Applicative f) => (a -> f b) -> e a -> f (e b)
 traverseDefault f = apply1 (fmap exists1 . traverse f)
 {-# INLINE traverseDefault #-}
 
-sequenceADefault :: (Existential1 e, ConstraintOf1 e ~ Traversable, Applicative f) => e (f a) -> f (e a)
+sequenceADefault :: (ExistentialWith1 Traversable e, Applicative f) => e (f a) -> f (e a)
 sequenceADefault = apply1 (fmap exists1 . sequenceA)
 {-# INLINE sequenceADefault #-}
 
-mapMDefault :: (Existential1 e, ConstraintOf1 e ~ Traversable, Monad m) => (a -> m b) -> e a -> m (e b)
+mapMDefault :: (ExistentialWith1 Traversable e, Monad m) => (a -> m b) -> e a -> m (e b)
 mapMDefault f = apply1 (liftM exists1 . mapM f)
 {-# INLINE mapMDefault #-}
 
-sequenceDefault :: (Existential1 e, ConstraintOf1 e ~ Traversable, Monad m) => e (m a) -> m (e a)
+sequenceDefault :: (ExistentialWith1 Traversable e, Monad m) => e (m a) -> m (e a)
 sequenceDefault = apply1 (liftM exists1 . sequence)
 {-# INLINE sequenceDefault #-}
 
 -- * Data.Functor.Contravariant.Contravariant
-contramapDefault :: (Existential1 e, ConstraintOf1 e ~ Contravariant) => (a -> b) -> e b -> e a
+contramapDefault :: ExistentialWith1 Contravariant e => (a -> b) -> e b -> e a
 contramapDefault f = apply1 (exists1 . contramap f)
 
 -- * Data.Functor.Extend.Extend
-duplicateDefault :: (Existential1 e, ConstraintOf1 e ~ Extend) => e a -> e (e a)
+duplicateDefault :: ExistentialWith1 Extend e => e a -> e (e a)
 duplicateDefault = apply1 (exists1 . fmap exists1 . duplicate)
 
--- extendDefault :: (Existential1 e, ConstraintOf1 e ~ Extend) => (e a -> b) -> e a -> e b
+-- extendDefault :: ExistentialWith1 Extend e => (e a -> b) -> e a -> e b
 
 -- * Control.Comonad.Comonad
-extractDefault :: (Existential1 e, ConstraintOf1 e ~ Comonad) => e a -> a
+extractDefault :: ExistentialWith1 Comonad e => e a -> a
 extractDefault = apply1 extract
 
 -- * Control.Comonad.Env.Class.ComonadEnv
-askDefault :: (Existential1 e, ConstraintOf1 e ~ ComonadEnv env) => e a -> env
+askDefault :: ExistentialWith1 (ComonadEnv env) e => e a -> env
 askDefault = apply1 ask
 
 -- * Control.Comonad.Traced.Class.ComonadTraced
-traceDefault :: (Existential1 e, ConstraintOf1 e ~ ComonadTraced m) => m -> e a -> a
+traceDefault :: ExistentialWith1 (ComonadTraced m) e => m -> e a -> a
 traceDefault a = apply1 (trace a)
 
 -- * Control.Comonad.Store.Class.ComonadStore
-posDefault :: (Existential1 e, ConstraintOf1 e ~ ComonadStore s) => e a -> s
+posDefault :: ExistentialWith1 (ComonadStore s) e => e a -> s
 posDefault = apply1 pos
 
-peekDefault :: (Existential1 e, ConstraintOf1 e ~ ComonadStore s) => s -> e a -> a
+peekDefault :: ExistentialWith1 (ComonadStore s) e => s -> e a -> a
 peekDefault a = apply1 (peek a)
 
-peeksDefault :: (Existential1 e, ConstraintOf1 e ~ ComonadStore s) => (s -> s) -> e a -> a
+peeksDefault :: ExistentialWith1 (ComonadStore s) e => (s -> s) -> e a -> a
 peeksDefault f = apply1 (peeks f)
 
-seekDefault :: (Existential1 e, ConstraintOf1 e ~ ComonadStore s) => s -> e a -> e a
+seekDefault :: ExistentialWith1 (ComonadStore s) e => s -> e a -> e a
 seekDefault a = apply1 (exists1 . seek a)
 
-seeksDefault :: (Existential1 e, ConstraintOf1 e ~ ComonadStore s) => (s -> s) -> e a -> e a
+seeksDefault :: ExistentialWith1 (ComonadStore s) e => (s -> s) -> e a -> e a
 seeksDefault f = apply1 (exists1 . seeks f)
 
 -- * Data.Copointed.Copointed
-copointDefault :: (Existential1 e, ConstraintOf1 e ~ Copointed) => e a -> a
+copointDefault :: ExistentialWith1 Copointed e => e a -> a
 copointDefault = apply1 copoint
