4c6b09782d2750fedb909a9e838fa6cebd09810a
illissius <illissius@gmail.com>
2011-11-27 18:24:19 +0100

    add default methods for Contravariant, Copointed, Extend, and various Comonads TODO: figure out package version constraints

--- Data/Exists/Defaults.hs
+++ Data/Exists/Defaults.hs
@@ -5,13 +5,22 @@ module Data.Exists.Defaults where
 
 import Data.Exists.Internal
 
-import Control.Applicative (Applicative)
-import Control.Monad       (liftM)
-import Data.Monoid         (Monoid)
-import Data.Foldable
-import Data.Traversable
+import Prelude                        (String, Int, ShowS, Monad, (.))
+import Control.Applicative            (Applicative)
+import Control.Monad                  (liftM)
+import Data.Monoid                    (Monoid)
 
-import Prelude hiding (foldl, foldl1, foldr, foldr1, mapM, sequence)
+import Prelude                        (Show          (..),
+                                       Functor       (..))
+import Data.Foldable                  (Foldable      (..))
+import Data.Traversable               (Traversable   (..))
+import Data.Functor.Contravariant     (Contravariant (..))
+import Data.Functor.Extend            (Extend        (..))
+import Control.Comonad                (Comonad       (..))
+import Control.Comonad.Env.Class      (ComonadEnv    (..))
+import Control.Comonad.Traced.Class   (ComonadTraced (..))
+import Control.Comonad.Store.Class    (ComonadStore  (..))
+import Data.Copointed                 (Copointed     (..))
 
 -- * Prelude.Show
 showDefault :: (Existential e, ConstraintOf e ~ Show) => e -> String
@@ -69,3 +78,44 @@ sequenceDefault :: (Existential1 e, ConstraintOf1 e ~ Traversable, Monad m) => e
 sequenceDefault = apply1 (liftM exists1 . sequence)
 {-# INLINE sequenceDefault #-}
 
+-- * Data.Functor.Contravariant.Contravariant
+contramapDefault :: (Existential1 e, ConstraintOf1 e ~ Contravariant) => (a -> b) -> e b -> e a
+contramapDefault f = apply1 (exists1 . contramap f)
+
+-- * Data.Functor.Extend.Extend
+duplicateDefault :: (Existential1 e, ConstraintOf1 e ~ Extend) => e a -> e (e a)
+duplicateDefault = apply1 (exists1 . fmap exists1 . duplicate)
+
+-- extendDefault :: (Existential1 e, ConstraintOf1 e ~ Extend) => (e a -> b) -> e a -> e b
+
+-- * Control.Comonad.Comonad
+extractDefault :: (Existential1 e, ConstraintOf1 e ~ Comonad) => e a -> a
+extractDefault = apply1 extract
+
+-- * Control.Comonad.Env.Class.ComonadEnv
+askDefault :: (Existential1 e, ConstraintOf1 e ~ ComonadEnv env) => e a -> env
+askDefault = apply1 ask
+
+-- * Control.Comonad.Traced.Class.ComonadTraced
+traceDefault :: (Existential1 e, ConstraintOf1 e ~ ComonadTraced m) => m -> e a -> a
+traceDefault a = apply1 (trace a)
+
+-- * Control.Comonad.Store.Class.ComonadStore
+posDefault :: (Existential1 e, ConstraintOf1 e ~ ComonadStore s) => e a -> s
+posDefault = apply1 pos
+
+peekDefault :: (Existential1 e, ConstraintOf1 e ~ ComonadStore s) => s -> e a -> a
+peekDefault a = apply1 (peek a)
+
+peeksDefault :: (Existential1 e, ConstraintOf1 e ~ ComonadStore s) => (s -> s) -> e a -> a
+peeksDefault f = apply1 (peeks f)
+
+seekDefault :: (Existential1 e, ConstraintOf1 e ~ ComonadStore s) => s -> e a -> e a
+seekDefault a = apply1 (exists1 . seek a)
+
+seeksDefault :: (Existential1 e, ConstraintOf1 e ~ ComonadStore s) => (s -> s) -> e a -> e a
+seeksDefault f = apply1 (exists1 . seeks f)
+
+-- * Data.Copointed.Copointed
+copointDefault :: (Existential1 e, ConstraintOf1 e ~ Copointed) => e a -> a
+copointDefault = apply1 copoint
